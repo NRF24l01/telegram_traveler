4fe4780a4509314fd989cbb75f57299fc8258db3
snevr <s.nevrovsky@ya.ru>
2023-10-28 15:33:26 +0300

    audio send and choise plase

--- main.py
+++ main.py
@@ -3,32 +3,82 @@ import telebot
 from telebot import types
 from logger import Logger
 from time import time
-from config import start_mesage
+from config import start_mesage, help_message
 from ttoken import token
-from sql_control import create_user_if_exist, get_sity
+from json import dumps, loads
+from sql_control import create_user_if_exist, get_sity, get_day, get_plase, get_txt_foto_audio
 
 bot = telebot.TeleBot(token)
 logger = Logger(f"logs/{round(time())}.log")
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
+    if create_user_if_exist(message.from_user.id):
+        bot.send_message(message.from_user.id, "📀Аккаунт успешно создан)")
     logger.info(f"Start message ({message.text}) from {message.from_user.id}")
     bot.send_message(message.from_user.id, start_mesage)
+
+
+@bot.message_handler(content_types=['text'], commands=['listen'])
+def listen_comand_handler(message):
+    logger.info(f"Get message {message.text} from {message.from_user.username}({message.from_user.id})")
+
+    sityes = get_sity()
+    markup = types.InlineKeyboardMarkup()
+    for i in sityes:
+        json = f"{i[0]}-_-_"
+        button1 = types.InlineKeyboardButton(i[0], callback_data=json)
+        markup.add(button1)
+    bot.send_message(message.from_user.id, "Выбери город:", reply_markup=markup)
+
 
 @bot.message_handler(content_types=['text'])
 def text_handler(message):
     if create_user_if_exist(message.from_user.id):
         bot.send_message(message.from_user.id, "📀Аккаунт успешно создан)")
-    sitis = get_sity()
     logger.info(f"Get message {message.text} from {message.from_user.username}({message.from_user.id})")
-    keyboard = types.InlineKeyboardMarkup(row_width=5)
-    for i in sitis:
-        keyboard.add(types.InlineKeyboardButton(text=i, callback_data = i))
-    bot.send_message(message.from_user.id, f"Вы написали: {message.text}", reply_markup=keyboard)
+    bot.send_message(message.from_user.id, help_message)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    if call.data:
-        print(call.data)
+    logger.info(f"Get data '{call.data}' from {call.from_user.username}({call.from_user.id})")
+
+    data = call.data.split("-")
+    logger.info(data)
+
+    if data[0] != "_" and data[1] == "_":
+        logger.info("send 2 choise")
+        days = get_day(data)
+        markup = types.InlineKeyboardMarkup()
+        print(data)
+        for i in days:
+            json = f"{data[0]}-{i[0]}-_"
+            button1 = types.InlineKeyboardButton(i[0], callback_data=json)
+            markup.add(button1)
+        bot.send_message(call.from_user.id, "Выбери день:", reply_markup=markup)
+    elif data[0] != "_" and data[1] != "_" and data[2] == "_":
+        logger.info("send 3 choise")
+        plases = get_plase(data)
+        markup = types.InlineKeyboardMarkup()
+        print(data)
+        for i in plases:
+            json = f"{data[0]}-{data[1]}-{i[0]}"
+            button1 = types.InlineKeyboardButton(i[0], callback_data=json)
+            markup.add(button1)
+        bot.send_message(call.from_user.id, "Выбери место:", reply_markup=markup)
+    elif data[0] != "_" and data[1] != "_" and data[2] != "_":
+        logger.info("send about")
+        dt = get_txt_foto_audio(data)
+        bot.send_message(call.from_user.id, dt[0])
+
+        bot.send_message(call.from_user.id, "Попытка подгрузки файла")
+        audio = open(f'audio/{dt[1]}', 'rb')
+        bot.send_audio(call.from_user.id, audio)
+        audio.close()
+
+
+
 
 bot.polling(none_stop=True)
--- sql_control.py
+++ sql_control.py
@@ -1,6 +1,7 @@
 import sqlite3
 import config
 
+
 def create_user_if_exist(user_id):
     try:
         conn = sqlite3.connect(config.db_name)
@@ -8,11 +9,11 @@ def create_user_if_exist(user_id):
 
         # cursor.execute("INSERT INTO 'users' (user_id) VALUES ('1000')")
         rang = cursor.execute("SELECT rang FROM Users WHERE user_id == ?", (str(user_id),)).fetchall()
+        print(rang)
         if len(rang) == 0:
-            cursor.execute("INSERT INTO 'Users' (user_id, rang) VALUES (?,?)", (user_id, 0))
+            cursor.execute("INSERT INTO Users (user_id, rang) VALUES (?,?)", (user_id, 0))
+            conn.commit()
             return 1
-
-        conn.commit()
 
     except sqlite3.Error as error:
         print("Error create sql: ", error)
@@ -22,6 +23,7 @@ def create_user_if_exist(user_id):
             conn.close()
     return 0
 
+
 def get_sity():
     try:
         conn = sqlite3.connect(config.db_name)
@@ -29,7 +31,9 @@ def get_sity():
         norm_sitys = []
 
         # cursor.execute("INSERT INTO 'users' (user_id) VALUES ('1000')")
-        sity = cursor.execute("SELECT sity FROM Plases").fetchall()[0]
+        sity = cursor.execute("SELECT sity FROM Plases").fetchall()
+        if len(sity) == 0:
+            return []
         for i in sity:
             if i not in norm_sitys:
                 norm_sitys.append(i)
@@ -43,3 +47,73 @@ def get_sity():
         if (conn):
             conn.close()
     return norm_sitys
+
+def get_day(data):
+    try:
+        conn = sqlite3.connect(config.db_name)
+        cursor = conn.cursor()
+        days = []
+
+        # cursor.execute("INSERT INTO 'users' (user_id) VALUES ('1000')")
+        print(data[0])
+        daY = cursor.execute("SELECT dayY FROM Plases WHERE sity == ?", (data[0],)).fetchall()
+        if len(daY) == 0:
+            return []
+        print(daY)
+        for i in daY:
+            if i not in days:
+                days.append(i)
+
+        conn.commit()
+
+    except sqlite3.Error as error:
+        print("Error create sql: ", error)
+
+    finally:
+        if (conn):
+            conn.close()
+    return days
+
+def get_plase(data):
+    try:
+        conn = sqlite3.connect(config.db_name)
+        cursor = conn.cursor()
+
+        days = []
+        dt = cursor.execute("SELECT plase FROM Plases WHERE sity == ? and dayY = ?", (data[0], data[1])).fetchall()
+        if len(dt) == 0:
+            return []
+        print(dt)
+        for i in dt:
+            if i not in days:
+                days.append(i)
+
+        conn.commit()
+    except sqlite3.Error as error:
+        print("Error create sql: ", error)
+
+    finally:
+        if (conn):
+            conn.close()
+    return days
+
+def get_txt_foto_audio(data):
+    try:
+        conn = sqlite3.connect(config.db_name)
+        cursor = conn.cursor()
+        print(data)
+
+        # cursor.execute("INSERT INTO 'users' (user_id) VALUES ('1000')")
+        print(data[0])
+        plase = cursor.execute("SELECT textik, audio_name FROM Plases WHERE sity == ? and dayY = ? and plase = ?", (data[0], data[1], data[2])).fetchall()[0]
+        print(plase)
+
+        conn.commit()
+
+    except sqlite3.Error as error:
+        print("Error create sql: ", error)
+
+    finally:
+        if (conn):
+            conn.close()
+    return plase
